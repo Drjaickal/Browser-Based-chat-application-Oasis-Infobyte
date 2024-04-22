1e35ef328e6b69332e3a562c9025b57d74050824
Drjaickal <105309360+Drjaickal@users.noreply.github.com>
2024-04-23 01:29:50 +0530

    uui part

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.css";
 import Home from "./pages/home/Home";
-import Login from "./pages/login/Login";
-import SignUp from "./pages/signup/SignUp";
+
 
 function App() {
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-      {/*<SignUp />*/}
       <Home/>
     </div>
   );
