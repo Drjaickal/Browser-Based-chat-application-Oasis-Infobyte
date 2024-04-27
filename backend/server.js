f64eb35a8805e097602be7de6e3b43cd6bdab6a2
Drjaickal <105309360+Drjaickal@users.noreply.github.com>
2024-04-27 17:16:46 +0530

    socket, sent message, receive and other things are done

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -6,29 +7,29 @@ import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
-
 import connectToMongoDB from "./db/connectToMongoDB.js";
-
-const app = express();
-const PORT = process.env.PORT || 5000;
+import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 
-app.use(express.json()); // to parse the incoming requests with JSON payloades (from req.body)
-app.use(cookieParser()); // to parse the incoming requests with cookie
+const __dirname = path.resolve();
+// PORT should be assigned after calling dotenv.config() because we need to access the env variables.
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-//app.get("/", (req, res) => {
-    //route route http://localhost:5000/
-    //res.send("Hello World")
-//});
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
-
-app.listen(PORT, () => {
-    connectToMongoDB()
-    console.log(`Server running on port ${PORT}`);
+server.listen(PORT, () => {
+	connectToMongoDB();
+	console.log(`Server Running on port ${PORT}`);
 });
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -6,10 +6,7 @@ const Home = () => {
     <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-grey-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
       <Sidebar />
       <MessageContainer />
-
     </div>
   );
-
 };
-
 export default Home;
